503e1c0a915066c3ede5ae741458a6f3c5b6c8ac
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-02-08 10:13:02 +0100

    hamburger manu color change

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const closeMenuBtn = document.querySelector("#close");
 const hamburgerBtn = document.querySelector("#open");
 const navigation = document.querySelector(".nav");
 const mask = document.querySelector("#mask");
+const allDivs = document.querySelectorAll("div");
 
 const openMenu = () => {
 	navigation.classList.add("active");
@@ -12,4 +13,20 @@ const closeMenu = () => {
 	mask.style.display = "";
 };
 
+const observer = () => {
+
+	const currentDiv = window.scrollY;
+	allDivs.forEach((div) => {
+		if (div.classList.contains("white") && div.offsetTop <= currentDiv + 36) {
+			hamburgerBtn.classList.add("white-color");
+		} else if (
+			!div.classList.contains("white") &&
+			div.offsetTop <= currentDiv + 36
+		) {
+			hamburgerBtn.classList.remove("white-color");
+		}
+	});
+};
+
 hamburgerBtn.addEventListener("click", openMenu);
+window.addEventListener("scroll", observer);
